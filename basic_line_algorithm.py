23d9e22acb2082288c7a9bd55d3dbffddc502819
rdpett99 <rdp06865@uga.edu>
2023-01-27 15:44:45 -0500

    tested drawing a vertical line

--- basic_line_algorithm.py
+++ basic_line_algorithm.py
@@ -7,12 +7,39 @@ assist with line drawing.
 
 # Standard Libraries
 import math
-import decimal
 import time
+import decimal
 
 # Python Imaging Library
 from PIL import Image
 
 # Creates an empty black image
 image = Image.new(mode="RGB", size = (250, 250), color = (0,0,0))
-# image.show()
+decimal.getcontext().prec = 5
+
+'''
+This function draws a line starting at point (x0, y0) and ending at point 
+(x1, y1) in a standard coordinate system.
+
+'''
+def draw_line(x0, y0, x1, y1):
+    # If x0 == x1 : the line is vertical
+    if x0 == x1 and y0 != y1:
+        # Need the smallest y value to know if line is going up or down
+        y_min = min(y0, y1)
+
+        # Starts the timer for the critical loop
+        start_time = decimal.Decimal(time.time())
+
+        # Critical loop
+        for y_coord in range(abs(y1 - y0)):
+            image.putpixel((x0, y_min + y_coord), (255, 255, 255))
+        
+        # Stops timer
+        end_time = decimal.Decimal(time.time())
+
+        # Debug
+        print(f'Loop took {end_time - start_time} seconds long.')
+        image.show()
+
+draw_line(50, 50, 50, 100)
